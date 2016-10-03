3aaf6a87703b41f35d94b8fa6cdcadf95ee09280
Christian Witts <cwitts@gmail.com>
2016-10-03 22:03:51 +0200

    Add SFML test

---
+++ SFML Game Dev/README.md
@@ -0,0 +1,1 @@
+Code samples from [SFML Game Development](https://www.packtpub.com/game-development/sfml-game-development)
---
+++ SFML Game Dev/test.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Chris on 2016/10/03.
+//
+
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Application");
+    sf::CircleShape shape;
+    shape.setRadius(40.f);
+    shape.setPosition(100.f, 100.f);
+    shape.setFillColor(sf::Color::Cyan);
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+}
