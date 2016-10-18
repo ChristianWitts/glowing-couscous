eb9810f1a07d9612218abd651145b34cd66dec7d
Christian Witts <cwitts@gmail.com>
2016-10-18 17:30:36 +0200

    Added the start of chapter 2

--- SFML Game Dev/Chapter 2/chapter2.cpp
+++ SFML Game Dev/Chapter 2/chapter2.cpp
@@ -4,94 +4,9 @@
 #include <SFML/Graphics.hpp>
 #include "chapter2.hpp"
 
-const sf::Time Game::TimePerFrame = sf::seconds(1.f / 60.f);
-
-Game::Game()
-	: mWindow(sf::VideoMode(640, 480), "SFML Application")
-	, mTexture()
-	, mPlayer()
-	, mIsMovingUp(false)
-	, mIsMovingDown(false)
-	, mIsMovingRight(false)
-	, mIsMovingLeft(false)
+namespace Textures
 {
-	if (!mTexture.loadFromFile("/Media/Textures/Eagle.png"))
-	{
-		// Handle loading error
-	}
-	mPlayer.setTexture(mTexture);
-	mPlayer.setPosition(100.f, 100.f);
-}
-
-void Game::run()
-{
-	sf::Clock clock;
-	sf::Time timeSinceLastUpdate = sf::Time::Zero;
-	while (mWindow.isOpen())
-	{
-		processEvents();
-		timeSinceLastUpdate += clock.restart();
-		while (timeSinceLastUpdate > TimePerFrame)
-		{
-			timeSinceLastUpdate -= TimePerFrame;
-			processEvents();
-			update(TimePerFrame);
-		}
-		render();
-	}
-}
-
-void Game::processEvents()
-{
-	sf::Event event;
-	while (mWindow.pollEvent(event))
-	{
-		switch (event.type)
-		{
-			case sf::Event::KeyPressed:
-				handlePlayerInput(event.key.code, true);
-				break;
-			case sf::Event::KeyReleased:
-				handlePlayerInput(event.key.code, false);
-			case sf::Event::Closed:
-				mWindow.close();
-				break;
-		}
-	}
-}
-
-void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
-{
-	if (key == sf::Keyboard::W)
-		mIsMovingUp = isPressed;
-	else if (key == sf::Keyboard::S)
-		mIsMovingDown = isPressed;
-	else if (key == sf::Keyboard::A)
-		mIsMovingLeft = isPressed;
-	else if (key == sf::Keyboard::D)
-		mIsMovingRight = isPressed;
-}
-
-void Game::update(sf::Time deltaTime)
-{
-	sf::Vector2f movement(0.f, 0.f);
-	if (mIsMovingUp)
-		movement.y -= 1.f;
-	if (mIsMovingDown)
-		movement.y += 1.f;
-	if (mIsMovingLeft)
-		movement.x -= 1.f;
-	if (mIsMovingRight)
-		movement.x += 1.f;
-
-	mPlayer.move(movement * deltaTime.asSeconds());
-}
-
-void Game::render()
-{
-	mWindow.clear();
-	mWindow.draw(mPlayer);
-	mWindow.display();
+	enum ID { Landscape, Airplane, Missile };
 }
 
 int main()
