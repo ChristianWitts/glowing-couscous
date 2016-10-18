fbefbafa8b46368db0a446437aee09ecbe851942
Christian Witts <cwitts@gmail.com>
2016-10-18 18:20:37 +0200

    Added section 001

---
+++ MOOCs/Udemy - Unreal Engine  Developer Course/001/Num001/Source/Num001/Num001GameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Num001.h"
+#include "Num001GameMode.h"
+
+
+
+
---
+++ MOOCs/Udemy - Unreal Engine  Developer Course/001/Num001/Source/Num001/Num001GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "Num001GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NUM001_API ANum001GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
