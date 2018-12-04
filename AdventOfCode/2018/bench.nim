cad50f878f4e2431a2c762847d8cdca3fe4dc572
Christian Witts <cwitts@gmail.com>
2018-12-04 13:53:54 +0200

    Add some helpers for AoC
    Signed-off-by: Christian Witts <cwitts@gmail.com>

---
+++ AdventOfCode/2018/bench.nim
@@ -0,0 +1,12 @@
+import times, os, strutils
+
+template benchmark(benchmarkName: string, code: untyped) =
+  block:
+    let t0 = epochTime()
+    code
+    let elapsed = epochTime() - t0
+    let elapsedStr = elapsed.formatFloat(format = ffDecimal, precision = 3)
+    echo "CPU Time [", benchmarkName, "] ", elapsedStr, "s"
+
+benchmark "my benchmark":
+  # call procs here
---
+++ AdventOfCode/2018/starter.nim
@@ -0,0 +1,20 @@
+# `nimble install itertools` for 3rd party dependencies
+# nim c -r -d:release
+
+import strutils
+import sequtils
+import math
+import sets
+
+# parse and store at compile time
+const lines = readFile("input").strip().splitlines()
+
+proc part1(): int =
+
+
+# proc part2(): int =
+
+
+when isMainModule:
+  echo(part1())
+  # echo(part2())
